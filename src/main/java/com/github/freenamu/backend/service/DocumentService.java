42ed08a5995e3db07885bdc34702de681043cff9
freeNAMU <freeNAMU@icloud.com>
2021-09-25 03:52:01 +0900

    Add Transactional annotation to DocumentService

--- src/main/java/com/github/freenamu/backend/service/DocumentService.java
+++ src/main/java/com/github/freenamu/backend/service/DocumentService.java
@@ -5,6 +5,7 @@ import com.github.freenamu.backend.entity.Document;
 import com.github.freenamu.backend.repository.DocumentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class DocumentService {
     @Autowired
     private DocumentRepository documentRepository;
